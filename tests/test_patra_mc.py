4a2cf0c9646f09fe8015ce3d8b6e25cd1f12561e
neelk <neelesh.karthi@gmail.com>
2024-09-10 13:34:16 -0400

    revert lints

--- tests/test_patra_mc.py
+++ tests/test_patra_mc.py
@@ -1,10 +1,8 @@
-from patra_model_card.patra_model_card import (AIModel, BiasAnalysis,
-                                               ExplainabilityAnalysis, Metric,
-                                               ModelCard, ModelCardJSONEncoder)
+from patra_model_card.patra_model_card import *
 import json
 import unittest
-from jsonschema import validate, exceptions
-import os
+from jsonschema import validate
+import os.path
 
 SCHEMA_JSON = os.path.join(os.path.dirname(__file__), os.pardir,
                            'patra_model_card/schema/schema.json')
@@ -13,19 +11,20 @@ SCHEMA_JSON = os.path.join(os.path.dirname(__file__), os.pardir,
 class ModelCardTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.aimodel = AIModel(name="DenseNet",
-                               version="v0.1",
-                               description="DenseNet CNN model",
-                               owner="PyTorch",
-                               location="pytorch.org",
-                               license="testLicence",
-                               framework="tensorflow",
-                               model_type="cnn",
-                               test_accuracy=0.89)
+
+        self.aimodel = AIModel(
+            name="DenseNet",
+            version="v0.1",
+            description="DenseNet CNN model",
+            owner="PyTorch",
+            location="pytorch.org",
+            license="testLicence",
+            framework="tensorflow",
+            model_type="cnn",
+            test_accuracy=0.89
+        )
         self.bias_analysis = BiasAnalysis(0.1, 0.2)
-        self.xai_analysis = ExplainabilityAnalysis(
-            "XAI Test 2", [Metric("xai1", "0.5"),
-                           Metric("xai2", "0.8")])
+        self.xai_analysis = ExplainabilityAnalysis("XAI Test 2", [Metric("xai1", 0.5), Metric("xai2", 0.8)])
 
         self.mc = ModelCard(
             name="icicle-camera-traps",
@@ -40,12 +39,14 @@ class ModelCardTestCase(unittest.TestCase):
             category="classification",
             ai_model=self.aimodel,
             bias_analysis=self.bias_analysis,
-            xai_analysis=self.xai_analysis)
+            xai_analysis=self.xai_analysis
+        )
         self.mc_save_location = "./test_mc.json"
 
     def test_json_serialization(self):
         """
-        This tests whether the python classes can be serialized to JSON.
+        This tests the serialization of the Model Card object.
+        :return:
         """
         # Convert the dataclass object to JSON string using the custom encoder
         mc_json = json.dumps(self.mc, cls=ModelCardJSONEncoder, indent=2)
@@ -55,33 +56,34 @@ class ModelCardTestCase(unittest.TestCase):
 
         self.assertEqual(parsed_mc['name'], "icicle-camera-traps")
         self.assertEqual(parsed_mc['ai_model']['name'], "DenseNet")
-        self.assertEqual(
-            parsed_mc['bias_analysis']['demographic_parity_difference'], 0.1)
+        self.assertEqual(parsed_mc['bias_analysis']['demographic_parity_difference'], 0.1)
 
     def test_schema_compatibility(self):
         """
-        This tests whether the serialized JSON is compatible with the schema.
+        This tests whether the python classes are inline with the provided model card schema.
+        :return:
         """
         # Convert the dataclass object to JSON string using the custom encoder
         mc_json = json.dumps(self.mc, cls=ModelCardJSONEncoder, indent=4)
-
         with open(SCHEMA_JSON, 'r') as schema_file:
             schema = json.load(schema_file)
         try:
             validate(json.loads(mc_json), schema)
-        except exceptions.ValidationError as e:
-            self.fail(f"Validation Error: {e}")
+        except:
+            self.fail("Validation Error!")
 
     def test_validate(self):
         """
         This tests the validate function.
+        :return:
         """
         is_valid = self.mc.validate()
         self.assertTrue(is_valid)
 
     def test_framework_enum(self):
         """
-        This tests the framework enum.
+        This tests the validate function.
+        :return:
         """
         mc = ModelCard(
             name="icicle-camera-traps",
@@ -93,18 +95,21 @@ class ModelCardTestCase(unittest.TestCase):
             input_data="cifar10",
             input_type="image",
             output_data="None",
-            category="classification")
-
-        aimodel = AIModel(name="DenseNet",
-                          version="v0.1",
-                          description="DenseNet CNN model",
-                          owner="PyTorch",
-                          location="pytorch.org",
-                          license="testLicence",
-                          framework="keras",
-                          model_type="dnn",
-                          test_accuracy=0.89)
+            category="classification"
+        )
+        aimodel = AIModel(
+            name="DenseNet",
+            version="v0.1",
+            description="DenseNet CNN model",
+            owner="PyTorch",
+            location="pytorch.org",
+            license="testLicence",
+            framework="keras",
+            model_type="dnn",
+            test_accuracy=0.89
+        )
         bias_analysis = BiasAnalysis(0.1, 0.2)
+        xai_analysis = ExplainabilityAnalysis("XAI Test 2", [Metric("xai1", 0.5), Metric("xai2", 0.8)])
 
         mc.ai_model = aimodel
         mc.bias_analysis = bias_analysis
@@ -114,8 +119,10 @@ class ModelCardTestCase(unittest.TestCase):
 
     def test_json_conversion(self):
         """
-        This tests if the JSON conversion works
+        This tests data insertion via converting it to json, converting back to python.
+        :return:
         """
+
         # Convert the dataclass object to JSON string using the custom encoder
         mc_json = json.dumps(self.mc, cls=ModelCardJSONEncoder, indent=4)
 
@@ -126,21 +133,19 @@ class ModelCardTestCase(unittest.TestCase):
 
     def test_json_save(self):
         """
-        This tests if the JSON file can be saved
+        This tests if the MC save as json works
+        :return:
         """
         self.mc.save(self.mc_save_location)
 
         # file existing verification
-        self.assertTrue(os.path.exists(self.mc_save_location),
-                        "File not found")
+        self.assertTrue(os.path.exists(self.mc_save_location), "File not found")
 
         # Verify the content in the file is same as the saved data
         with open(self.mc_save_location, 'r') as json_file:
             saved_data = json.load(json_file)
 
-        self.assertEqual(
-            saved_data['name'], self.mc.name,
-            "Saved JSON data doesn't match the original Model Card")
+        self.assertEqual(saved_data['name'], self.mc.name, "Saved JSON data doesn't match the original Model Card")
 
     def tearDown(self):
         # Removing the saved file for the model card
