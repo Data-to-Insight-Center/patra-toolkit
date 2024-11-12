f0144e64e610972033860b21070dfc5c4afa8e60
neelk <neelesh.karthi@gmail.com>
2024-11-11 21:52:51 -0500

    move files

---
+++ docs/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Patra Model Card Toolkit'
+copyright = '2024, Data To Insights Center, Indiana University'
+author = 'Data To Insights Center, Indiana University'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',   # Automatically generate documentation from docstrings
+    'sphinx.ext.napoleon',  # Support for Google-style docstrings
+    'sphinx.ext.autosummary'
+]
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))
+print("sys.path: ", sys.path)
+
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
