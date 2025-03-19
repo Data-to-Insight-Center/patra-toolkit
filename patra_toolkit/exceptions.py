be2c4a86826f338eb2f4ddc2ec7797afc8b52749
neelk <neelesh.karthi@gmail.com>
2025-03-19 13:43:19 -0400

    remove requirements

--- patra_toolkit/exceptions.py
+++ patra_toolkit/exceptions.py
@@ -8,3 +8,8 @@ class PatraSubmissionError(Exception):
     """Exception raised when model submission to Patra Server fails. """
     def __init__(self, message):
         super().__init__(message)
+
+class PatraModelExistsError(Exception):
+    """Exception raised when model already exists in the Patra Knowledge Graph."""
+    def __init__(self, message):
+        super().__init__(message)
